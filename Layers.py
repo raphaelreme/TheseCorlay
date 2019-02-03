bee74c961fb5110622324ebbf4707518accd7179
Raphael REME <raphael.reme@orange.fr>
2019-02-03 21:00:32 +0100

    Debut du code de layers, a tester

---
+++ Layers.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+import mxnet as mx
+from mxnet import nd, autograd
+
+
+
+class Layers:
+    """One layers of a neural net.
+
+    Has a n neurones (size of the output) and the p inputs.
+    There is therefore n(p+1) parameters (weigths + bias).
+
+    One can precise each activation function for each neurone.
+    One can also fixed a set of connections for the learning algorithm."""
+
+    def __init__(self, output_size, input_size, function = nd.sigmoid, ctx = mx.cpu(0), fixed = False):
+        if output_size < 1 and input_size < 1:
+            raise ValueError("Sizes must be positive integer")
+
+        self.ctx = ctx
+
+        weights = nd.random.normal(loc = 0, scale = 1, shape = (outut_size, input_size), ctx = self.ctx))
+        bias = nd.random.normal(loc = 0, scale = 1, shape = output_size, ctx = self.ctx)
+
+        ###############################
+        ###### Compute functions ######
+        try:
+            if len(function) != output_size: # If len is defined, we test that it's a good one, otherwise there is a TypeError and we deal with it in the except bloc
+                raise ValueError("'function' argument can either be one function for all the neurones or n functions (one for each)")
+            self.function_is_one = False
+            self.function = function
+        except TypeError:
+            self.function_is_one = True
+            self.function = function
+
+
+        ###########################################
+        ###### Compute the fixed parameters #######
+
+        message = "'fixed' argument can either be one boolean for all the parameters or n booleans, one for each neurone and their connections, or p+1 booleans, one for each input (+bias, give the bias first) and their connections or a matrix of n*(p+1) booleans for each connection"
+        try:
+            if len(fixed.shape) == 1:
+                if fixed.shape[0] = input_size + 1:
+                    self.bias_fixed = nd.full(self.bias.shape, fixed[0], ctx = self.ctx)
+                    self.weights_fixed = nd.dot(nd.full((output_size, 1), 1), fixed[1:].reshape((1, input_size))).copyto(self.ctx)
+                elif fixed.shape[0] = output_size:
+                    self.bias_fixed = fixed.copyto(self.ctx)
+                    self.weights_fixed = nd.dot(fixed.reshape((output_size, 1)), nd.full((1, input_size))).copyto(self.ctx)
+                else:
+                    raise ValueError(message)
+            elif fixed.shape = (output_size, input_size + 1):
+                self.bias_fixed = fixed[:,0].copyto(self.ctx)
+                self.weights_fixed = fixed[:,1:].copyto(self.ctx)
+            else:
+                raise ValueError(message)
+        except TypeError:
+            self.weights_fixed = nd.full(self.weights.shape, fixed, ctx = self.ctx)
+            self.bias_fixed = nd.full(self.bias.shape, fixed, ctx = self.ctx)
+
+        self.bias_fixed = self.bias_fixed.astype('int8')
+        self.weights_fixed = self.weights_fixed.astype('int8')
+
+
+    def compute(input):
+        Z = np.dot(self.weights, input) + self.bias
+
+        if self.function_is_one:
+            return self.function(Z)
+        else
+            A = Z.zeros_like()
+            for i in range(len(Z)):
+                for j in range(len(Z[0])): #If input is of shape p * batch_size or of size p it still work
+                    A[i][j] = self.function[i](Z[i][j])
--- NeuralNet.py
+++ NeuralNet.py
@@ -3,21 +3,23 @@
 import mxnet as mx
 from mxnet import nd, autograd
 
+from Layers import Layers
+
 
 class NeuralNet:
     """Represent a neural network able to correct a code.
-    
+
     Can be trained and used on data.
     Can also be stored in file or create from a file"""
-    
+
     ###########Training characteristics and methods####################
     nbIter = 50
     batchSize = 500
-    
+
     def SE(yhat,y):
         return nd.sum((yhat - y) ** 2)
     SE = staticmethod(SE)
-    
+
     ##pourrait etre une methode d'objet
     def adam(params, vs, sqrs, maximums, lr, batch_size, t):
         beta1 = 0.9
@@ -39,65 +41,65 @@ class NeuralNet:
             param[:] = param - div
             param[:] = nd.where(param > maximum, maximum, param)
             param[:] = nd.where(param < -maximum, -maximum, param)
-            
-            
-    
+
+
+
     adam = staticmethod(adam)
     ###############################################
-    
-    
-    
-        
+
+
+
+
     def __init__(self, layersNumber, sizes = None, layers = None, ctx = None, fct = ampliOP, maximum = 1):
         self.ctx = ctx
         if ctx:
             self.ctx = ctx
 
-            
+
         self.fct = fct
-        
+
         self.layersNumber = insideLayersNumber
         self.sizes = sizes
         if sizes == None:
-            #Mettre les 
-        
+            #Mettre les
+
         self.params = list()
         self.maximums = []
-        
+
         for i in range(self.layersNumber+1):
             self.params.append(nd.random_normal(loc = 0,scale = 0.05,shape=(self.sizes[i],self.sizes[i+1]),ctx=self.ctx))
             self.params.append(nd.random.normal(loc = 0,scale = 0.05, shape = (self.sizes[i+1],),ctx=self.ctx))
             self.maximums.append(nd.array([[maximum for l in range(self.sizes[i+1])] for k in range(self.sizes[i])]))
             self.maximums.append(nd.array([maximum for l in range(self.sizes[i+1])]))
-        
-        
+
+
         self.t = 1
         self.vs = []
         self.sqrs = []
-        
+
         for param in self.params:
             param.attach_grad()
             self.vs.append(param.zeros_like())
             self.sqrs.append(param.zeros_like())
-        
+
         self.lr = 0.001
-    
-    
+
+
     def size(self):
         S = 0
         for param in self.params:
             S+= param.size
         return S
-    
+
     ##Peut etre rajouter une autre methode qui fait le round en sortie
     def net(self,input):
         L = input
         for i in range(self.layersNumber+1):
             L = self.fct(nd.dot(L,self.params[2*i])+self.params[2*i+1])
         return L
-        
-    
-    
+
+
+
     def train(self,epochs):
         for i in range(epochs):
             efficiency = 0
@@ -110,8 +112,8 @@ class NeuralNet:
                 noiseBSC = nd.floor(noiseBSC/nd.max(noiseBSC,axis=(1,)).reshape((self.batchSize,1)))
                 actif = nd.array([[random.uniform(0,1)>0.125]*self.code.n for k in range(self.batchSize)], ctx = self.ctx)
                 noiseBSC = noiseBSC * actif
-                
-                
+
+
                 y = (x + noiseBSC)%2
 
                 with autograd.record():
@@ -121,22 +123,22 @@ class NeuralNet:
 
                 self.adam(self.params,self.vs,self.sqrs, self.maximums, self.lr, self.batchSize, self.t)
                 self.t+=1
-        
+
                 cumuLoss += loss.asscalar()
                 zHat = nd.round(zHat)
                 efficiency += nd.sum(nd.equal(zHat,z)).asscalar()
 
-                
+
             Pc = efficiency/(self.batchSize*self.nbIter*self.code.k)
             Pe = 1 - Pc
             normCumuLoss = cumuLoss/(self.batchSize*self.nbIter*self.code.k)
             print("Epochs %d: Pe = %lf , loss = %lf" % (i,Pe,normCumuLoss))
-    
+
     def train2(self,epochs):
         batchSize = self.code.size
         x = self.code.F
         z = self.code.E
-        
+
         for i in range(epochs):
             efficiency = 0
             cumuLoss = 0
@@ -145,7 +147,7 @@ class NeuralNet:
                 noiseBSC = nd.floor(noiseBSC/nd.max(noiseBSC,axis=(1,)).reshape((batchSize,1)))
                 actif = nd.array([[random.uniform(0,1)>0.125]*self.code.n for k in range(batchSize)], ctx = self.ctx)
                 noiseBSC = noiseBSC * actif
-                
+
                 y = (x + noiseBSC)%2
 
                 with autograd.record():
@@ -155,17 +157,17 @@ class NeuralNet:
 
                 self.adam(self.params,self.vs,self.sqrs, self.maximums, self.lr, batchSize, self.t)
                 self.t+=1
-        
+
                 cumuLoss += loss.asscalar()
                 zHat = nd.round(zHat)
                 efficiency += nd.sum(nd.equal(zHat,z)).asscalar()
 
-               
+
             Pc = efficiency/(batchSize*self.nbIter*self.code.k)
             Pe = 1 - Pc
             normCumuLoss = cumuLoss/(batchSize*self.nbIter*self.code.k)
             print("Epochs %d: Pe = %lf , loss = %lf" % (i,Pe,normCumuLoss))
-    
+
     def train3(self,epochs):
         batchSize = len(self.code.reachableWords)
         z = []
@@ -173,7 +175,7 @@ class NeuralNet:
             z.extend([list(elt)]*(self.code.n+1))
         z = nd.array(z,ctx=self.ctx)
         x = self.code.reachableWords
-        
+
         for i in range(epochs):
             efficiency = 0
             cumuLoss = 0
@@ -182,21 +184,21 @@ class NeuralNet:
                     zHat = self.net(x)
                     loss = self.SE(zHat,z)
                 loss.backward()
-                
+
                 self.adam(self.params,self.vs,self.sqrs, self.maximums, self.lr, batchSize, self.t)
                 self.t+=1
-        
+
                 cumuLoss += loss.asscalar()
                 zHat = nd.round(zHat)
                 efficiency += nd.sum(nd.equal(zHat,z)).asscalar()
- 
+
             Pc = efficiency/(batchSize*self.nbIter*self.code.k)
             Pe = 1 - Pc
             normCumuLoss = cumuLoss/(batchSize*self.nbIter*self.code.k)
             print("Epochs %d: Pe = %lf , loss = %lf" % (i,Pe,normCumuLoss))
 
-            
-            
+
+
     def computePerformances(self):
         wrong = []
         cpt = 0
@@ -208,31 +210,31 @@ class NeuralNet:
                         wrong.append((self.code.E[i],word))
                         cpt+=1
                         break
-        
+
         return (cpt,len(self.code.reachableWords),wrong)
-                
-    
+
+
     ##overwrite the file ./file
     def save(self,file):
         with open(file,"w") as f:
             f.write(self.toString())
-    
+
     def toString(self):
         s = "/Dimension :\n\n"
         s += str(self.layersNumber) + "\n"
         for size in self.sizes:
             s += str(size) + " "
-    
+
         G = self.code.G.asnumpy()
         s += "\n\n/Code :\n"
         for ligne in G:
             s+="\n"
             for value in ligne:
                 s+=str(value) + " "
-            
-        
+
+
         s+="\n\n/Parameters :\n"
-        
+
         for param in self.params:
             s+="\n"
             for ligne in param:
@@ -241,43 +243,43 @@ class NeuralNet:
                 s+="\n"
         return s
 
-    
+
     def open(cls, file, ctx = mx.cpu(0)):
         s = ""
         with open(file,"r") as f:
             s = f.read()
         return cls.stringToNet(s, ctx = ctx)
     open = classmethod(open)
-    
+
     def stringToNet(cls, s, ctx = mx.cpu(0)):
         tab = s.split("\n")
         tab2 = []
         for chain in tab:
             if "/" not in chain and chain != "":
                 tab2.append(chain.strip(" "))
-        
+
         insideLayersNumber = int(tab2.pop(0))
         sizes = []
-        
+
         for chain in tab2.pop(0).split(" "):
             sizes.append(int(chain))
         k = sizes[-1]
         n = sizes[0]
         G = nd.array([[0 for j in range(n)] for i in range(k)],ctx=mx.cpu(0))
-            
+
         for i in range(k):
             ligne = tab2.pop(0).split(" ")
             for j in range(n):
                 G[i,j] = float(ligne[j])
-        
+
         code = Code(G, ctx)
         net = cls(code,insideLayersNumber,sizes[1:-1], ctx)
-        
+
         for param in net.params:
             for ligne in param:
                 ligne_fichier = tab2.pop(0).split(" ")
                 for i in range(len(ligne_fichier)):
                     ligne[i] = float(ligne_fichier[i])
-        
+
         return net
     stringToNet = classmethod(stringToNet)
